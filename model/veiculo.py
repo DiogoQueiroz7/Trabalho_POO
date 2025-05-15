de23fbb876005594bd636fbe9714e592ca97af1b
Gabriel-Guanais <laiteguanais@gmail.com>
2025-05-14 21:43:53 -0400

    Add classe veiculo com propriedades encapsuladas

---
+++ model/veiculo.py
@@ -0,0 +1,39 @@
+class Veiculo:
+    def __init__(self, id, placa, modelo, capacidade_carga):
+        self.__id = id
+        self.__placa = placa
+        self.__modelo = modelo
+        self.__capacidade_carga = capacidade_carga
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def placa(self):
+        return self.__placa
+
+    @placa.setter
+    def placa(self, value):
+        self.__placa = value
+
+    @property
+    def modelo(self):
+        return self.__modelo
+
+    @modelo.setter
+    def modelo(self, value):
+        self.__modelo = value
+
+    @property
+    def capacidade_carga(self):
+        return self.__capacidade_carga
+
+    @capacidade_carga.setter
+    def capacidade_carga(self, value):
+        self.__capacidade_carga = value
+
